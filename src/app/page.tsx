c112ef21e38274848b7f0d310426522565bd4efa
Mohsen Ahmed <mohesnm36@gmail.com>
2024-10-19 11:31:48 +0300

    change colour Text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Home() {
     return (
         <div className="font-roboto">
-            <h1 className="text-3xl font-bold">This is First One</h1>
+            <h1 className="text-6xl font-bold text-green-600">This is First One</h1>
             <Link href="/about" className="text-blue-500 underline">About Link</Link>
         </div>
     );
